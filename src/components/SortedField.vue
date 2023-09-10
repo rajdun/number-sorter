97ba176bbf0371979b9ac7130a67ff35e9f935e4
Rajdun <=>
2023-09-10 20:54:04 +0200

    sorting and fixes

---
+++ src/components/SortedField.vue
@@ -0,0 +1,77 @@
+<template>
+  <div :class="{ 'align-left': alignLeft, 'align-right': !alignLeft }" id="sorted-field" @mousedown="onClick">
+    <div class="index-field" :class="{ 'available': value < 0, 'picked': value > 0 }">{{ index + 1 }}</div>
+    <div class="value-field" v-if="value > 0">{{ value }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'SortedField',
+  setup() {
+    return {
+    };
+  },
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    alignLeft: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  emits: ['onMouseDown'],
+  methods: {
+    onClick() {
+      this.$emit('onMouseDown', this.index);
+    },
+  },
+});
+</script>
+
+<style lang="stylus" scoped>
+#sorted-field
+  display: flex
+  margin: 0.5rem 0
+  align-items: center
+  justify-content: space-between
+  cursor: pointer
+  width: 100%
+.index-field
+  width: 2rem
+  height: 2rem
+  padding: 0.2rem
+  display: flex
+  justify-content: center
+  align-items: center
+  border-radius: 0.7rem
+  font-size: 1.5rem
+  font-weight: bold
+  margin: 0 0.5rem
+.value-field
+  line-height: 2rem
+.available
+  background-color: #fff
+  color: #000
+.picked
+  border: 1px solid #fff
+.align-right
+  flex-direction: row-reverse
+  justify-content: flex-start
+  text-align: left
+.align-left
+  flex-direction: row
+  justify-content: flex-end
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="game">
     <NavBar />
-    <RandomNumberDisplay :randomNumber="randomNumber" @mousedown="onMouseDown" @transition="transitionChanged"
+    <RandomNumberDisplay :randomNumber="randomNumber" @mousedown="onRandomNumberClick" @transition="transitionChanged"
       ref="randomNumberDisplayRef" />
+
+    <div class="sorted-field-container">
+      <div class="sortable-fields">
+        <SortedField v-for="(value, index) in sortedNumbers.slice(0, 5)" :key="index" :index="index" :value="value"
+          :alignLeft="true" @onMouseDown="onSordetFieldClick" />
+      </div>
+      <div class="sortable-fields">
+        <SortedField v-for="(value, index) in sortedNumbers.slice(5, 10)" :key="index" :index="index + 5" :value="value"
+          @onMouseDown="onSordetFieldClick" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,12 +21,21 @@
 import { defineComponent, ref } from 'vue';
 import RandomNumberDisplay from '@/components/RandomNumberDisplay.vue';
 import NavBar from '@/components/NavBar.vue';
+import SortedField from '@/components/SortedField.vue';
+
+// eslint-disable-next-line no-shadow
+enum GameStates {
+  Shuffling,
+  Sorting,
+  Won,
+}
 
 export default defineComponent({
   name: 'GameView',
   components: {
     NavBar,
     RandomNumberDisplay,
+    SortedField,
   },
   setup() {
     const randomNumberDisplayRef = ref<InstanceType<typeof RandomNumberDisplay> | null>(null);
@@ -27,13 +47,15 @@ export default defineComponent({
     return {
       randomNumber: 1000,
       isTransitioning: false,
+      sortedNumbers: Array.from(Array(10)).fill(-1),
+      gameState: GameStates.Shuffling,
     };
   },
   methods: {
-    onMouseDown() {
-      if (!this.isTransitioning) {
+    onRandomNumberClick() {
+      if (!this.isTransitioning && this.gameState === GameStates.Shuffling) {
         this.randomNumber = Math.floor(Math.random() * 1000);
-
+        this.gameState = GameStates.Sorting;
         if (this.randomNumberDisplayRef) {
           this.randomNumberDisplayRef.handleChange();
         }
@@ -42,9 +64,68 @@ export default defineComponent({
     transitionChanged(e: boolean) {
       this.isTransitioning = e;
     },
+    onSordetFieldClick(a: number) {
+      if (!this.canSelectNumber(a)) {
+        return;
+      }
+
+      this.sortedNumbers[a] = this.randomNumber;
+      this.gameState = GameStates.Shuffling;
+
+      if (!this.validateIfNumbersAreSorted()) {
+        console.log('You lost');
+      }
+    },
+    validateIfNumbersAreSorted() {
+      const numbers = this.sortedNumbers.filter((n) => n > 0);
+
+      for (let i = 0; i < numbers.length - 1; i += 1) {
+        if (numbers[i] > numbers[i + 1]) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+    canSelectNumber(index: number) {
+      if (this.sortedNumbers[index] > 0) {
+        return false;
+      }
+
+      if (this.isTransitioning) {
+        return false;
+      }
+
+      if (this.gameState !== GameStates.Sorting) {
+        return false;
+      }
+
+      return true;
+    },
   },
 });
 </script>
 
 <style lang="stylus" scoped>
+.sorted-field-container
+  display flex
+  justify-content space-between
+  margin-top 20px
+  margin-bottom 20px
+  margin-left auto;
+  margin-right auto;
+  .sortable-fields
+    display flex
+    flex-direction column
+    justify-content space-between
+  @media (max-width: 600px)
+    width: 100%
+  @media (min-width: 600px)
+    width: 100%
+  @media (min-width: 768px)
+    width: 90%
+  @media (min-width 922px)
+    width: 75%
+  @media (min-width: 1200px)
+    width: 60%
 </style>
