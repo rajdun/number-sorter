4efbfa07c8f0cba979a96a1fd6ef0f1cb418ad49
Rajdun <=>
2023-09-17 22:27:44 +0200

    config for deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/number-sorter/' : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       // eslint-disable-next-line no-param-reassign
